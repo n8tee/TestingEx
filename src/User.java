abce276f2a3bd2a64f9e2062af6c3dd4ee7cd06f
Nate Hug <nathaniel.hughes@my.utsa.edu>
2026-03-19 12:28:20 -0500

    this is my first commit for the repo

---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
