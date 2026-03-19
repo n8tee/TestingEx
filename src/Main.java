168d22f03d3fe651ec56f91faeb5e5d4d2b1de00
Nate Hug <nathaniel.hughes@my.utsa.edu>
2026-03-19 12:38:17 -0500

    this is the branch example

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is working");
+        System.out.println("This is working through the branch!");
     }
 }
